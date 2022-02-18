9ed55168669716789edf42f1ef0f3e85dd0845cc
Gustavo Jejer <43014008+Krieger93@users.noreply.github.com>
2022-02-18 13:39:06 -0300

    Update StoredProdecures.sql

--- StoredProdecures.sql
+++ StoredProdecures.sql
@@ -1,11 +1,15 @@
--- SP ejercicio 1
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ordenarinc`(IN $columna VARCHAR(30), IN $orden VARCHAR(4))
+-- SP ejercicio 1 lista por columna y orden
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ordenarinc`(IN campo VARCHAR(30), IN orden VARCHAR(4))
 BEGIN
-    DECLARE order_by varchar(50);
-    SET @order_by = CONCAT_WS(' ','`columna`', '`orden`');
-    SELECT * FROM `incidentes` ORDER BY @order_by;
+	--    DECLARE clausula varchar(50);
+    DECLARE clausula varchar(50);
+	-- @clausula ES LA CLAUSULA FINAL A EJECUTAR, EN LA QUE SE ENCADENAN LA SENTENCIA SQL Y LAS VARIABLES
+    SET @clausula = concat('SELECT * FROM incidentes ORDER BY ', campo , ' ', orden);
+    -- SE PREPARA, EJECUTA Y CIERRA LA SENTENCIA
+    PREPARE ejecutar FROM @clausula;
+    EXECUTE ejecutar;
+    DEALLOCATE PREPARE ejecutar;
 END
-
 
 --SP ejercicio 2 - actualiza estado de incidente
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_updateincestado`(IN i_id INT, IN i_estado varchar(25))
